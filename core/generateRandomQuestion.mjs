2bf018373e9c3196e586d85055c6ebc030b74075
neopool <persisne@gmail.com>
2024-01-31 23:45:23 +0900

    feat: generateRandomQuestion 실행 방식 변경 코드 추가

--- core/generateRandomQuestion.mjs
+++ core/generateRandomQuestion.mjs
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import readline from 'readline';
 import inquirer from 'inquirer';
 
 const QUESTIONS_DIR_PATH = 'interview-questions';
@@ -15,6 +16,9 @@ const __filename = fileURLToPath(import.meta.url);
 // __filename에서 디렉토리 경로를 얻음
 const __dirname = path.dirname(__filename);
 
+let allQuestions = [];
+let remainingQuestions = [];
+
 const getFilesFromDirectory = () => {
   const directoryPath = path.resolve(__dirname, `../${QUESTIONS_DIR_PATH}`);
   return fs.readdirSync(directoryPath);
@@ -27,10 +31,7 @@ const getFilesFromDirectory = () => {
  */
 const readQuestionsFromFile = (fileName) => {
   try {
-    const filePath = path.resolve(
-      __dirname,
-      `../${QUESTIONS_DIR_PATH}/${fileName}`
-    );
+    const filePath = path.resolve(__dirname, `../${QUESTIONS_DIR_PATH}/${fileName}`);
     const fileContents = fs.readFileSync(filePath, 'utf8');
     const questionsJson = JSON.parse(fileContents);
     return questionsJson.questions;
@@ -50,26 +51,55 @@ const selectRandomElements = (array, numElements = 3) => {
   return shuffled.slice(0, numElements);
 };
 
-const main = async () => {
-  const files = getFilesFromDirectory();
+/**
+ * 사용자가 엔터키를 누를 때까지 기다린다.
+ * @returns {Promise<void>}
+ **/
+const waitForEnter = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  const answers = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'selectedFile',
-      message: '질문 파일을 선택해주세요.',
-      choices: files,
-    },
-  ]);
-
-  const questions = readQuestionsFromFile(answers.selectedFile);
-  return selectRandomElements(questions);
+  return new Promise((resolve) =>
+    rl.question('계속하려면 엔터키를 눌러주세요...👍', (ans) => {
+      rl.close();
+      resolve();
+    })
+  );
 };
 
-main()
-  .then((questions) => {
-    console.log('❤️질문 입니다.❤️\n ', questions);
-  })
-  .catch((error) => {
-    console.error('😅에러가 발생하였어요.😅\n ', error);
-  });
+const main = async () => {
+  if (!allQuestions.length) {
+    const files = getFilesFromDirectory();
+    const answers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'selectedFile',
+        message: '질문 파일을 선택해주세요.',
+        choices: files,
+      },
+    ]);
+    allQuestions = readQuestionsFromFile(answers.selectedFile);
+    remainingQuestions = [...allQuestions];
+  }
+
+  while (remainingQuestions.length) {
+    const numElements = Math.min(3, remainingQuestions.length);
+    const selectedQuestions = selectRandomElements(remainingQuestions, numElements);
+    console.log('❤️질문 입니다.❤️\n ', selectedQuestions);
+
+    // Remove the selected questions from the remaining questions
+    remainingQuestions = remainingQuestions.filter((question) => !selectedQuestions.includes(question));
+
+    if (remainingQuestions.length) {
+      await waitForEnter();
+    }
+  }
+
+  console.log('모든 질문이 선택되었습니다. 다음에 또 만나요!😎');
+};
+
+main().catch((error) => {
+  console.error('😅에러가 발생하였어요.😅\n ', error);
+});
